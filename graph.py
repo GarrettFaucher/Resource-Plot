0a78b9e9c26ba3825d756cc69fdf40cc3815888f
Garrett Faucher <garrettfaucher@gmail.com>
2019-12-03 21:10:55 -0500

    Documentation

--- graph.py
+++ graph.py
@@ -1,14 +1,16 @@
-import matplotlib.pyplot as plt #TODO: Install matplotlib using pip
-import pandas as pd #TODO: Install pandas using pip
+import matplotlib.pyplot as plt # Install matplotlib using pip
+import pandas as pd # Install pandas using pip
 import sys
 
 # Store command line arguments in variables
-filename = sys.argv[1]
-type = sys.argv[2]
-color = sys.argv[3]
+filename = sys.argv[1] # Name of file to be used
+type = sys.argv[2] # Type of graph to be used
+color = sys.argv[3] # Color of points to be used
 
 df = pd.read_csv('../' + filename) # Importing a DataFrame from CSV file
 
+# Determining what graph will look like
+# One column needs to be named x, and another y
 if type == 'bar':
     df.plot(kind='bar',x='x',y='y',color=color)
 elif type == 'line':
