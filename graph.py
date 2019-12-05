fd9acb950b1808242292ce6e1449bc22a4df32f9
Garrett Faucher <garrettfaucher@gmail.com>
2019-12-05 17:52:31 -0500

    Different plot methods

--- graph.py
+++ graph.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt # Install matplotlib using pip
+import matplotlib.animation as animation
 import pandas as pd # Install pandas using pip
 import sys
 
@@ -7,15 +8,26 @@ filename = sys.argv[1] # Name of file to be used
 type = sys.argv[2] # Type of graph to be used
 color = sys.argv[3] # Color of points to be used
 
-df = pd.read_csv('../' + filename) # Importing a DataFrame from CSV file
+fig = plt.figure()
+axis1 = fig.add_subplot(1,1,1)
 
-# Determining what graph will look like
-# One column needs to be named x, and another y
-if type == 'bar':
-    df.plot(kind='bar',x='x',y='y',color=color)
-elif type == 'line':
-    df.plot(kind='line',x='x',y='y',color=color)
-elif type == 'scatter':
-    df.plot(kind='scatter',x='x',y='y',color=color)
+def animate(i):
+    data = open('../' + filename, 'r').read()
+    lines = data.split('\n')
+    xs = []
+    ys = []
+    for line in lines:
+        if len(line) > 1:
+            x, y = line.split(',')
+            xs.append(x)
+            ys.append(y)
+    axis1.clear()
+    if type == 'bar':
+        axis1.bar(xs, ys, color=color)
+    elif type == 'line':
+        axis1.plot(xs, ys, color=color)
+    elif type == 'scatter':
+        axis1.scatter(xs, ys, color=color)
 
+ani = animation.FuncAnimation(fig, animate, interval=100)
 plt.show()
